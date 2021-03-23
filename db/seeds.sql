6491dd81145ec50ae7b98b2c9e873b24e5a9b4f6
Cody Regis <codyregis6891@yahoo.com>
2021-03-22 18:49:46 -0700

    added schema and seed files for sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ('Coaching Staff'),
+       ('Training Staff'),
+       ('Athletics'),
+       ('Nutrition'),
+       ('Media'),
+       ('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Head Coach', 1000000, 1),
+       ('Hitting Coach', 500000, 1),
+       ('Pitching Coach', 500000, 1),
+       ('Strength Coordinator', 100000, 2),
+       ('Conditioning Coordinator', 100000, 2),
+       ('Team Physician', 250000, 2),
+       ('Pitcher', 10000000, 3),
+       ('Infielder', 5000000, 3),
+       ('Outfielder', 15000000, 3),
+       ('Dietician', 50000, 4),
+       ('Chef', 75000, 4),
+       ('Play-by-Play Commentator', 200000, 5),
+       ('PA Announcer', 45000, 5),
+       ('Ticket Sales', 35000, 6),
+       ('Usher', 35000, 6),
+       ('Marketing', 115000, 6);
